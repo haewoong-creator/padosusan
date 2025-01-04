1f8f6180f8404289d896a77bbcec9b22b9eeb76f
haewoong-creator <ehw0626@naver.com>
2025-01-04 17:19:43 +0900

    Add files via upload
    간단한 연락처

--- main.dart
+++ main.dart
@@ -1,110 +1,58 @@
-
 import 'package:flutter/material.dart';
-
-
 void main() {
   runApp(const MyApp());
 }
-
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(//구글이 제공하는 Material테마 이용할 수 있음,아이폰 기본앱은 cupertino~~
-      home: Scaffold(//상중하로 나눠줌
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-         actions: [Icon(Icons.search),
-         Padding(
-           padding: const EdgeInsets.only(left:16.0),
-           child: Icon(Icons.menu),
-         ),Padding(
-           padding: const EdgeInsets.only(left:16.0),
-           child: Icon(Icons.notifications),
-         ),],
-          title: Text('정자동'),
-          titleTextStyle: TextStyle(color: Colors.black,fontSize: 20),
+          backgroundColor: Colors.lightBlue,
         ),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          height: 200, width: double.infinity,
-
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body:SizedBox(
+          child:ListView(
             children: [
-              Expanded(
-              flex: 1,
-              child: Container(
-                child: Image.asset('assets/dang.JPG',
-                height: 200,
-                fit: BoxFit.fill,
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(Icons.person,color: Colors.white,),
                 ),
-                height: 200, width: 200,
-
+                title: Text('홍길동'),
               ),
-              ),
-              Expanded(
-                flex: 2,
-              child:Container(
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                  flex:2,
-                    child:Container(
-                      width: double.infinity,
-                      child: Text('캐논 DSLR 100D(단렌즈, 충전기 16기가SD 포함)',
-                      style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),
-                      ),
-
-                    ),
-        ),
-        Expanded(
-            flex:1,
-            child:Container(
-              width: double.infinity,
-              child: Text('율전동 • 끌올 10분전',
-              style: TextStyle(color: Colors.grey),),
-    ),
-    ),
-    Expanded(
-    flex:1,
-    child:Container(
-      width: double.infinity,
-      child: Text('30,0000원'),
-
-    ),
-    ),
-                Expanded(
-                    flex:1,
-                    child:Container(
-                      width: double.infinity,
-
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Icon(Icons.favorite_border),
-                    Text('4')
-                  ],
-
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  child: Icon(Icons.person,color: Colors.white,),
                 ),
-
-              ),
-        ),
-                  ],
-                ),
-              ),
-              ),
+                title: Text('홍길동'),
+              )
             ],
           ),
-
-
         ),
-
-        ),
-      );
+        bottomNavigationBar: Bottom()
+      ),
+    );
   }
 }
+class Bottom extends StatelessWidget {
+  const Bottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child:  Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(Icons.phone),
+          Icon(Icons.message),
+          Icon(Icons.person)
+        ],
+      ),
+    );
+  }
+}
+
 
